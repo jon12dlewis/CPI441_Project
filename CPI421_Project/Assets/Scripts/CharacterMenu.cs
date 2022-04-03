73b4dd1366092e0dae91dbc6de546ba9e85e1356
jon12dlewis <jon12dlewis@gmail.com>
2022-04-03 00:07:12 -0700

    weapon upgrading in progress

--- CPI421_Project/Assets/Scripts/CharacterMenu.cs
+++ CPI421_Project/Assets/Scripts/CharacterMenu.cs
@@ -7,6 +7,14 @@ public class CharacterMenu : MonoBehaviour
 {
     public Text healthText, blueCrystalText, yellowCrystalText, redCrystalText;
     public player player; 
+    public Sprite[] pickAxeImage, weaponImage, bowImage;
+    private Sprite pickAxeDisplay, weaponDisplay, bowDisplay;
+    public Weapon weapon;
+    public PickAxe pickAxe;
+
+    int weaponSelected = 1;
+    int pickAxeSelected = 1;
+    int bowSelected = 1;
 
     // Update is called once per frame
     public void UpdateMenu()
@@ -16,4 +24,38 @@ public class CharacterMenu : MonoBehaviour
         yellowCrystalText.text = GameManager.instance.yellow_crystals.ToString();;
         redCrystalText.text = GameManager.instance.red_crystals.ToString();;
     }
+
+    public void increaseWeapon()
+    {
+        weaponSelected += 1;
+        if(weaponSelected > 3)
+        {
+            weaponSelected = 1;
+        }
+    }
+
+    public void decreaseWeapon()
+    {
+        weaponSelected -= 1;
+        if(weaponSelected < 1)
+        {
+            weaponSelected = 3;
+        }
+    }
+
+    public void UpdateSword()
+    {
+        weaponDisplay = weaponImage[weaponSelected-1];
+    }
+
+    public void UpdateBow()
+    {
+
+    }
+
+    public void UpdatePickAxe()
+    {
+
+    }
+
 }
