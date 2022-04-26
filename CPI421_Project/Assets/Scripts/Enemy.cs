9e54b35d03ac61345a9a04922f061360951cdc2b
jon12dlewis <jon12dlewis@gmail.com>
2022-04-26 11:14:45 -0700

    Added arrow drop to the enemies,

--- CPI421_Project/Assets/Scripts/Enemy.cs
+++ CPI421_Project/Assets/Scripts/Enemy.cs
@@ -16,6 +16,7 @@ public class Enemy : Mover
     private Transform playerTransform;
     private Vector3 startingPosition;
     public GameObject heart;
+    public GameObject arrowDrop;
 
     // AI
     public Transform target;
@@ -196,28 +197,28 @@ public class Enemy : Mover
                 chasing = false;
             }
 
-            if(agentSpeed.x > 0.2f)
+            if(agentSpeed.x > 0.1f)
             {
                 direction.x = 1;
                 direction.y = 0;
                 facing = 2;
             }
             else
-            if(agentSpeed.x < -0.2f)
+            if(agentSpeed.x < -0.1f)
             {
                 direction.x = -1;
                 direction.y = 0;
                 facing = 3;
             }
             else
-            if(agentSpeed.y > 0.2f)
+            if(agentSpeed.y > 0.1f)
             {
                 direction.x = 0;
                 direction.y = 1;
                 facing = 0;
             }
             else
-            if(agentSpeed.y < -0.2f)
+            if(agentSpeed.y < -0.1f)
             {
                 direction.x = 0;
                 direction.y = -1;
@@ -241,6 +242,7 @@ public class Enemy : Mover
     {
         musicController.EnemyDisable(this.gameObject);
         int val = Random.Range(1, 5);
+        int val2 = Random.Range(1, 5);
         if(val == 1)
         {
             Vector3 newRot = new Vector3(0,0,0);
@@ -248,10 +250,17 @@ public class Enemy : Mover
             Instantiate(heart, transform.position, rot);
         }
 
+        if(val2 == 4)
+        {
+            Vector3 newRot = new Vector3(0,0,0);
+            Quaternion rot = Quaternion.Euler(newRot); 
+            Instantiate(arrowDrop, transform.position, rot);
+        }
+
 
         Destroy(gameObject);
-        GameManager.instance.expierence += xpValue;
-        GameManager.instance.ShowText("+ " + xpValue + "xp", 60, Color.magenta, transform.position, Vector3.up * 40, 1.0f);
+        //GameManager.instance.expierence += xpValue;
+        //GameManager.instance.ShowText("+ " + xpValue + "xp", 60, Color.magenta, transform.position, Vector3.up * 40, 1.0f);
     }
 
     void ShootUp()
--- CPI421_Project/Assets/Scripts/Scorpion.cs
+++ CPI421_Project/Assets/Scripts/Scorpion.cs
@@ -16,6 +16,7 @@ public class Scorpion : Mover
     private Transform playerTransform;
     private Vector3 startingPosition;
     public GameObject heart;
+    public GameObject arrowDrop;
 
     // AI
     public Transform target;
@@ -198,15 +199,23 @@ public class Scorpion : Mover
     protected override void Death()
     {
         Destroy(gameObject);
-        GameManager.instance.expierence += xpValue;
-        GameManager.instance.ShowText("+ " + xpValue + "xp", 60, Color.magenta, transform.position, Vector3.up * 40, 1.0f);
+        //GameManager.instance.expierence += xpValue;
+        //GameManager.instance.ShowText("+ " + xpValue + "xp", 60, Color.magenta, transform.position, Vector3.up * 40, 1.0f);
 
         int val = Random.Range(1, 5);
+        int val2 = Random.Range(1, 5);
         if(val == 1)
         {
             Vector3 newRot = new Vector3(0,0,0);
             Quaternion rot = Quaternion.Euler(newRot); 
             Instantiate(heart, transform.position, rot);
+        }
+
+        if(val2 == 4)
+        {
+            Vector3 newRot = new Vector3(0,0,0);
+            Quaternion rot = Quaternion.Euler(newRot); 
+            Instantiate(arrowDrop, transform.position, rot);
         }
 
     }
