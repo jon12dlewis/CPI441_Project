67bdaaeddac626b6c61ebdc301f2e2f861046d71
jon12dlewis <jon12dlewis@gmail.com>
2022-04-26 14:56:01 -0700

    Fixed issue where chests gave the player 10 crystals consistently

--- CPI421_Project/Assets/Scripts/Chest.cs
+++ CPI421_Project/Assets/Scripts/Chest.cs
@@ -8,6 +8,12 @@ public class Chest : Collectable
     public int crystalAmount = 5;
     [SerializeField] AudioSource coin1;
     [SerializeField] AudioSource positive;
+    private player player;
+
+    void start()
+    {
+        player = GameObject.Find("Player").GetComponent<player>();
+    }
 
 
     // Start is called before the first frame update
@@ -18,7 +24,36 @@ public class Chest : Collectable
                 collected = true;
                 GetComponent<SpriteRenderer>().sprite = emptyChest;
                 transform.localScale = new Vector3(0.3f,0.3f,1);
-                GameManager.instance.GiveBlueCrystal(crystalAmount);
+
+                int yellow_crystals_chance = Random.Range(1 + player.getDiscovery() , 10);
+                int blue_crystals_chance = Random.Range(1 + player.getDiscovery() , 20);
+                int red_crystals_chance = Random.Range(1 + player.getDiscovery() , 50);
+                int arrow_chance = Random.Range(1 + player.getDiscovery() , 20);
+
+                if(yellow_crystals_chance == 9)
+                {
+                    int crystalAmount = Random.Range(1 + player.getDiscovery() , 10 + player.getDiscovery());
+                    GameManager.instance.GiveYellowCrystal(crystalAmount);
+                }
+
+                if(blue_crystals_chance == 19)
+                {
+                    int crystalAmount = Random.Range(1 + player.getDiscovery() , 7 + player.getDiscovery());
+                    GameManager.instance.GiveBlueCrystal(crystalAmount);
+                }
+
+                if(red_crystals_chance == 49)
+                {
+                    int crystalAmount = Random.Range(1 + player.getDiscovery() , 5 + player.getDiscovery());
+                    GameManager.instance.GiveRedCrystal(crystalAmount);
+                }
+
+                if(arrow_chance == 19)
+                {
+                    GameManager.instance.arrows += 5;
+                }
+
+                
                 //Debug.Log("Grant " + mulaAmount + " mula!");
                 GameManager.instance.ShowText("+ " + crystalAmount + " Crystals!", 25, Color.yellow, transform.position , Vector3.up * 25, 1.0f);
 
